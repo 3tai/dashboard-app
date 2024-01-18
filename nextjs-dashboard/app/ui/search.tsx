bdf93cdebbeeda95b6d05eeeed4ff3963c622186
3tai <nitsniko3@gmail.com>
2024-01-18 23:31:24 +0530

    add pagination

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -1,24 +1,27 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   // next/navigation APIs
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  // handleSearch function using search params
-  function handleSearch(term: string) {
+  // refactored handleSearch function to arrow function while using debounce
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching...${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
   // search component
   return (
     <div className="relative flex flex-1 flex-shrink-0">
